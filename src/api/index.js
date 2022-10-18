b95496eac4bf98df14364c2d81ba585eaa30e332
tobaomoi <imminhat@gmail.com>
2022-10-18 20:14:06 +0700

    last-update

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const callApi = axios.create({
-    baseURL: "https://192.168.1.7:8082/api"
-    // baseURL: "https://localhost:5001/api"
+    baseURL: "https://book-store-demoo.herokuapp.com/api"
 })
 
 export default callApi;
