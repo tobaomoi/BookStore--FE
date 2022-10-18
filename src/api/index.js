64cf6fa9e6f770c8b73ca27f2c3ec3ab6335384c
tobaomoi <imminhat@gmail.com>
2022-10-18 18:47:00 +0700

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const callApi = axios.create({
-    baseURL: "https://192.168.1.7:5001/api"
+    baseURL: "https://192.168.1.7:8082/api"
     // baseURL: "https://localhost:5001/api"
 })
 
